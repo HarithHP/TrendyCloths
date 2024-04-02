8462a5fcfe5b8c3e3d659ccc7e080adf240a0de3
HarithHP <hphadaragama@gmail.com>
2024-04-02 23:54:27 +0530

    login and sign up view

---
+++ TrendyCloths/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  TrendyCloths
+//
+//  Created by User on 2024-04-02.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
