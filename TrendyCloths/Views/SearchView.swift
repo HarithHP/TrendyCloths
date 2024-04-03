3d005273a3c6591d6462e7acd3df2d89aca0fb70
HarithHP <hphadaragma@gmail.com>
2024-04-03 22:00:54 +0530

    Checkout View

---
+++ TrendyCloths/Views/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  TrendyCloths
+//
+//  Created by User on 2024-04-03.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
